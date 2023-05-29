408c999beca68d2e38b6b10ca471efb2d2c9b672
lutzzdias <thiagolutzdias@hotmail.com>
2023-05-29 02:35:44 -0400

    feat: initialize project and setup styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export function Header() {
   return (
     <h1>Hello World</h1>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,48 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: 'var(--font-source-sans-pro)',
+        alt: 'var(--font-montserrat)',
+      },
+      colors: {
+        red: {
+          300: '#f08979',
+          500: '#e94f37',
+          600: '#d44832',
+        },
+        purple: {
+          50: '#f5eef8',
+          100: '#e0cce8',
+          200: '#d1b3dd',
+          300: '#bd90cd',
+          400: '#b07ac4',
+          500: '#9c59b5',
+          600: '#8e51a5',
+          700: '#6f3f81',
+          800: '#563164',
+          900: '#42254c',
+        },
+        gray: {
+          50: '#e9e9e9',
+          100: '#b9b9bb',
+          200: '#98989a',
+          300: '#68686c',
+          400: '#4b4b50',
+          500: '#1e1e24',
+          600: '#1b1b21',
+          700: '#15151a',
+          800: '#111114',
+          900: '#0d0d0f',
+        },
+        white: {
+          50: '#fefeff',
+          100: 'fdfdff',
+          200: '#fcfbff',
+          300: '#fafaff',
+          400: '#f9f9ff',
+          500: '#f8f7ff',
+        }
       },
     },
   },

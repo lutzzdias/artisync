fec15b8b471d97397aae06f316bc299fe231c14e
lutzzdias <thiagolutzdias@hotmail.com>
2023-05-29 10:59:20 -0400

    fix: search bar styling and html

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,36 @@
 import Image from 'next/image'
-import { Plus } from 'lucide-react'
+import { Plus, Search } from 'lucide-react'
 
 import logo from '@/assets/logo.svg'
 
 export function Header() {
   return (
-    <header className="flex flex-row justify-between items-center p-4">
+    <header className="flex flex-row items-center justify-between p-4">
       {/* logo */}
-      <div className="flex flex-row font-alt text-3xl gap-x-3 font-semibold">
-        <Image src={logo} alt="" width={40} height={40} />  
+      <div className="flex flex-row gap-3 font-alt text-3xl font-semibold">
+        <Image src={logo} alt="" width={40} height={40} />
         ArtiSync
       </div>
 
       {/* actions */}
-      <div className='flex flex-row gap-x-4'>
+      <div className="flex flex-row gap-4">
         {/* search area */}
         <section role="search">
-          <form action="#" method="get">
-            <label htmlFor="search">
-              <input type="search" name="search" placeholder="Search by author or title" maxLength={200} />
-            </label>
+          <form action="#" method="get" className="w-80">
+            <div className="flex items-center gap-2 rounded-xl border border-solid border-gray-300 px-4">
+              <Search size={24} />
+              <input
+                className="w-full bg-transparent py-3 focus:outline-none"
+                type="search"
+                placeholder="Search by author or title"
+                maxLength={200}
+              />
+            </div>
           </form>
         </section>
         {/* create article button */}
-        <button className='bg-purple-500 text-white-500 rounded-xl py-3 px-4 align-middle justify-center flex flex-row'>
-          <Plus size={16} />
+        <button className="flex flex-row items-center justify-center gap-2 rounded-xl bg-purple-500 px-4 py-3 text-white-500">
+          <Plus size={24} />
           Create article
         </button>
       </div>

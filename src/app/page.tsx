42c6b36ee397d0cfe531f64dc72ec4beaf996b45
lutzzdias <thiagolutzdias@hotmail.com>
2023-05-29 11:39:21 -0400

    feat: base structure for home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,28 @@
+import { Card } from '@/components/Card'
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <div className="flex flex-col gap-5">
+      {/* header */}
+      <div className="flex items-center justify-between">
+        <h2 className="text-3xl font-semibold">Articles</h2>
+        <h4 className="text-sm">
+          Sort by:{' '}
+          <button className="text-gray-300 hover:text-gray-200" type="button">
+            Author
+          </button>
+        </h4>
+      </div>
+
+      {/* grid */}
+      <div className="grid grid-cols-4 gap-8">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
   )
 }

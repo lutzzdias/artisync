396dd218a342225801121c751eae968c183f1030
lutzzdias <thiagolutzdias@hotmail.com>
2023-08-04 14:14:11 -0400

    refactor: descriptive function naming

--- src/components/ArticleButton.tsx
+++ src/components/ArticleButton.tsx
@@ -7,15 +7,13 @@ import { ArticleModal } from './ArticleModal'
 export function ArticleButton() {
   const [showModal, setShowModal] = useState(false)
 
-  const createArticle = () => {
-    setShowModal(true)
-  }
+  const openCreationModal = () => setShowModal(true)
 
   return (
     <>
       <button
         className="flex flex-row items-center justify-center gap-2 rounded-xl bg-purple-500 px-4 py-3 text-white-500"
-        onClick={createArticle}
+        onClick={openCreationModal}
       >
         <Plus size={24} />
         Create article

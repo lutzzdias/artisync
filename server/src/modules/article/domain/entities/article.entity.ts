e29fc9c73086aeca199479c071bb0590eeb86ceb
lutzzdias <thiagolutzdias@hotmail.com>
2023-08-11 14:25:02 -0400

    feat: add article entity attributes

--- server/src/modules/article/domain/entities/article.entity.ts
+++ server/src/modules/article/domain/entities/article.entity.ts
@@ -1,2 +1,10 @@
-// TODO: Add properties and methods
-export class Article {}
+export class Article {
+  id: string;
+  title: string;
+  author: string; // TODO: change string -> Author entity
+  description: string;
+  link: string;
+  state: string; // TODO: change string -> Enum value
+  createdAt: Date;
+  updatedAt: Date;
+}

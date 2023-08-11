882a7d73851de9b3c36e2a4022cb2d9ed4917d80
lutzzdias <thiagolutzdias@hotmail.com>
2023-08-11 18:19:08 -0400

    feat[server]: implement getById with typeorm (not 100%)

--- server/src/modules/article/domain/interfaces/article.repository.interface.ts
+++ server/src/modules/article/domain/interfaces/article.repository.interface.ts
@@ -1,10 +1,11 @@
 import { Article } from '../entities/article.entity';
+import { IArticleSchema } from './article.schema.interface';
 
 // TODO: Create generic repository (?)
 export interface IArticleRepository {
-  create(article: Article): Promise<Article>;
-  update(id: string, article: Article): Promise<Article>;
+  create(article: Article): Promise<IArticleSchema | void>;
+  update(id: string, article: Article): Promise<IArticleSchema | void>;
   delete(articleId: string): Promise<void>;
-  getById(articleId: string): Promise<Article>;
-  getAll(): Promise<Array<Article>>;
+  getById(articleId: string): Promise<IArticleSchema | void>;
+  getAll(): Promise<Array<IArticleSchema>>;
 }
--- server/src/modules/article/repository/typeorm/repository/article.repository.ts
+++ server/src/modules/article/repository/typeorm/repository/article.repository.ts
@@ -1,21 +1,31 @@
+import { InjectRepository } from '@nestjs/typeorm';
 import { Article } from 'src/modules/article/domain/entities/article.entity';
 import { IArticleRepository } from 'src/modules/article/domain/interfaces/article.repository.interface';
+import { Repository } from 'typeorm';
+import { ArticleSchema } from '../schema/article.schema';
 
 // TODO: Implement with decided ORM or database
 export class ArticleRepository implements IArticleRepository {
-  create(article: Article): Promise<Article> {
+  constructor(
+    @InjectRepository(ArticleSchema)
+    private readonly articleRepository: Repository<ArticleSchema>,
+  ) {}
+
+  async create(article: Article): Promise<ArticleSchema | void> {
     throw new Error('Method not implemented.');
   }
-  update(id: string, article: Article): Promise<Article> {
+  async update(id: string, article: Article): Promise<ArticleSchema | void> {
     throw new Error('Method not implemented.');
   }
-  delete(articleId: string): Promise<void> {
+  async delete(articleId: string): Promise<void> {
     throw new Error('Method not implemented.');
   }
-  getById(articleId: string): Promise<Article> {
-    throw new Error('Method not implemented.');
+  async getById(articleId: string): Promise<ArticleSchema | void> {
+    const options = { where: { id: articleId } };
+    const articleSchema = await this.articleRepository.findOne(options);
+    return articleSchema;
   }
-  getAll(): Promise<Article[]> {
+  async getAll(): Promise<Array<ArticleSchema>> {
     throw new Error('Method not implemented.');
   }
 }

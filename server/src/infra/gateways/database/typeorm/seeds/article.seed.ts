27f1d13d55fcce59dbfd2fae35c3225624694f96
lutzzdias <thiagolutzdias@hotmail.com>
2023-08-23 02:47:22 -0400

    feat [server]: implement article seed and factory

---
+++ server/src/infra/gateways/database/typeorm/seeds/article.seed.ts
@@ -0,0 +1,14 @@
+import { DataSource } from 'typeorm';
+import { Seeder, SeederFactoryManager } from 'typeorm-extension';
+
+import { ArticleSchema } from 'src/modules/article/repository/typeorm/schema/article.schema';
+
+export default class ArticleSeed implements Seeder {
+  public async run(
+    dataSource: DataSource,
+    factoryManager: SeederFactoryManager,
+  ): Promise<any> {
+    const articleFactory = await factoryManager.get(ArticleSchema);
+    await articleFactory.saveMany(10);
+  }
+}

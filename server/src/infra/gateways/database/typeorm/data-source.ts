1785254b534f1a20494b18181257d305e11f2744
lutzzdias <thiagolutzdias@hotmail.com>
2023-08-23 02:52:24 -0400

    fix [server]: import statement outside module fixed

--- server/src/infra/gateways/database/typeorm/data-source.ts
+++ server/src/infra/gateways/database/typeorm/data-source.ts
@@ -8,7 +8,7 @@ export const dataSourceOptions: DataSourceOptions & SeederOptions = {
   username: process.env.DB_USER || 'admin',
   password: process.env.DB_PASSWORD || 'admin',
   database: process.env.DB_NAME || 'artisync-db',
-  entities: ['dist/**/*.schema.{js,ts}', 'src/**/*.schema.{js,ts}'], // TODO: check why __dirname does not work here
+  entities: ['dist/**/*.schema.{js,ts}'], // TODO: check why __dirname does not work here
   migrations: [`${__dirname}/**/migrations/**/*.{js,ts}`],
   seeds: [`${__dirname}/**/seeds/**/*.{js,ts}`],
   factories: [`${__dirname}/**/factories/**/*.{js,ts}`],

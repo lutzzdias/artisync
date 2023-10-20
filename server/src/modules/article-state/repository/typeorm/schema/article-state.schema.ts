b3d75807f4adc069442d59de29a9225b8be01f67
lutzzdias <thiagolutzdias@hotmail.com>
2023-10-20 21:28:31 +0100

    feat [server]: implement articleState schema

---
+++ server/src/modules/article-state/repository/typeorm/schema/article-state.schema.ts
@@ -0,0 +1,32 @@
+import { IArticleStateSchema } from 'src/modules/article-state/domain/interfaces/article-state.schema.interface';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'ArticleState' })
+export class ArticleStateSchema implements IArticleStateSchema {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 255 })
+  name: string;
+
+  @Column({ length: 510 })
+  description: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  @CreateDateColumn()
+  createdAt?: Date;
+
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  @UpdateDateColumn()
+  updatedAt?: Date;
+}

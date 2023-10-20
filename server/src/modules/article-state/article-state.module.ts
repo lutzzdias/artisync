5669c7af8bafdcb587c20d37b3c7f6c4531da895
lutzzdias <thiagolutzdias@hotmail.com>
2023-10-20 22:01:59 +0100

    fix [server]: impoert typeOrm in articleState module

--- server/src/modules/article-state/article-state.module.ts
+++ server/src/modules/article-state/article-state.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ArticleStateController } from './controller/article-state.controller';
+import { ArticleStateSchema } from './repository/typeorm/schema/article-state.schema';
 import { ArticleStateService } from './service/article-state.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ArticleStateSchema])],
   controllers: [ArticleStateController],
   providers: [ArticleStateService],
 })

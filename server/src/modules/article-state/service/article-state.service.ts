09961c74e4b221dc038266d8223c2a4be4a9724e
lutzzdias <thiagolutzdias@hotmail.com>
2023-10-20 21:45:54 +0100

    feat [server]: implement articleState service

--- server/src/modules/article-state/service/article-state.service.ts
+++ server/src/modules/article-state/service/article-state.service.ts
@@ -1,26 +1,70 @@
 import { Injectable } from '@nestjs/common';
-import { CreateArticleStateDto } from './dtos/create-article-state.dto';
-import { UpdateArticleStateDto } from './dtos/update-article-state.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ArticleState } from '../domain/entities/article-state.entity';
+import { IArticleStateRepository } from '../domain/interfaces/article-state.repository.interface';
+import { CreateArticleStateDto } from '../dtos/create-article-state.dto';
+import { UpdateArticleStateDto } from '../dtos/update-article-state.dto';
+import { ArticleStateRepository } from '../repository/typeorm/repository/article-state.repository';
+import { ArticleStateSchema } from '../repository/typeorm/schema/article-state.schema';
 
 @Injectable()
 export class ArticleStateService {
-  create(createArticleStateDto: CreateArticleStateDto) {
-    return 'This action adds a new articleState';
+  private repository: IArticleStateRepository;
+
+  constructor(
+    @InjectRepository(ArticleStateSchema)
+    private readonly typeormRepository: Repository<ArticleStateSchema>,
+  ) {
+    this.repository = new ArticleStateRepository(typeormRepository);
   }
 
-  findAll() {
-    return `This action returns all articleState`;
+  async create(createArticleStateDto: CreateArticleStateDto) {
+    // Convert from DTO to Entity
+    const articleState = ArticleState.fromCreateArticleStateDto(
+      createArticleStateDto,
+    );
+
+    // TODO: Add logic
+
+    // Convert from Entity to Schema
+    const articleStateSchema = ArticleState.toSchema(articleState);
+
+    // Save to the database
+    return await this.repository.create(articleStateSchema);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} articleState`;
+  async getAll() {
+    // TODO: Add logic
+    return await this.repository.getAll();
   }
 
-  update(id: number, updateArticleStateDto: UpdateArticleStateDto) {
-    return `This action updates a #${id} articleState`;
+  async getById(id: string) {
+    // TODO: Add logic
+    return await this.repository.getById(id);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} articleState`;
+  async update(id: string, updateArticleStateDto: UpdateArticleStateDto) {
+    const articleStateSchema = await this.repository.getById(id);
+
+    if (articleStateSchema) {
+      // Convert from Schema to Entity
+      const oldArticleState = ArticleState.fromSchema(articleStateSchema);
+      // Update old entity with new values (from DTO)
+      const updatedArticleState = oldArticleState.fromUpdateArticleStateDto(
+        updateArticleStateDto,
+      );
+      // Save to the database
+      return await this.repository.update(id, updatedArticleState);
+    } else {
+      // Return error
+      return new ArticleState();
+    }
+  }
+
+  async delete(id: string) {
+    // TODO: Add logic
+    // TODO: Add a return (success or error)
+    return await this.repository.delete(id);
   }
 }

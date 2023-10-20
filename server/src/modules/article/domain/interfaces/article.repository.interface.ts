d4f10d9592442060ccb86f31407c7de14e8ecfa5
lutzzdias <thiagolutzdias@hotmail.com>
2023-10-21 00:47:53 +0100

    fix [server]: rename parameter related to the id

--- server/src/modules/article/domain/interfaces/article.repository.interface.ts
+++ server/src/modules/article/domain/interfaces/article.repository.interface.ts
@@ -4,7 +4,7 @@ import { IArticleSchema } from './article.schema.interface';
 export interface IArticleRepository {
   create(article: IArticleSchema): Promise<IArticleSchema | void>;
   update(id: string, article: IArticleSchema): Promise<IArticleSchema | void>;
-  delete(articleId: string): Promise<void>;
-  getById(articleId: string): Promise<IArticleSchema | void>;
+  delete(id: string): Promise<void>;
+  getById(id: string): Promise<IArticleSchema | void>;
   getAll(): Promise<Array<IArticleSchema>>;
 }

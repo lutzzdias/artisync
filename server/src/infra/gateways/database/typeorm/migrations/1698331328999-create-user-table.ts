651c8f5811ec976743fae10fafd5057c15793ee0
lutzzdias <thiagolutzdias@hotmail.com>
2023-10-26 15:48:37 +0100

    feat [server]: create user table migration

---
+++ server/src/infra/gateways/database/typeorm/migrations/1698331328999-create-user-table.ts
@@ -0,0 +1,69 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUserTable1698331328999 implements MigrationInterface {
+  private table = new Table({
+    name: 'User',
+    columns: [
+      {
+        name: 'id',
+        type: 'uuid',
+        isPrimary: true,
+        isNullable: false,
+        isUnique: true,
+        generationStrategy: 'uuid',
+        default: 'uuid_generate_v4()',
+      },
+      {
+        name: 'email',
+        type: 'varchar',
+        isNullable: false,
+        isUnique: true,
+      },
+      {
+        name: 'username',
+        type: 'varchar',
+        isNullable: false,
+        isUnique: true,
+      },
+      {
+        // TODO: make it safe eg. hash it
+        name: 'password',
+        type: 'varchar',
+      },
+      {
+        name: 'bio',
+        type: 'varchar',
+        length: '510',
+      },
+      {
+        name: 'lastLogin',
+        type: 'timestamp',
+      },
+      {
+        name: 'deletedDefaultStates',
+        type: 'text',
+        isArray: true,
+        isNullable: true,
+      },
+      {
+        name: 'createdAt',
+        type: 'timestamp',
+        default: 'CURRENT_TIMESTAMP',
+      },
+      {
+        name: 'updatedAt',
+        type: 'timestamp',
+        default: 'CURRENT_TIMESTAMP',
+        onUpdate: 'CURRENT_TIMESTAMP',
+      },
+    ],
+  });
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(this.table);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable(this.table);
+  }
+}

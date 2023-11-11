a5224be3acd333e6f49e7ca8e4ed787359b9f62f
lutzzdias <thiagolutzdias@hotmail.com>
2023-11-11 03:33:11 +0000

    test [server]: implement basic user service unit tests

--- server/src/modules/user/service/user.service.spec.ts
+++ server/src/modules/user/service/user.service.spec.ts
@@ -1,18 +1,67 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Repository } from 'typeorm';
+import { User } from '../entity/user.entity';
+import { UserRepository } from '../repository/user.repository';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
-    let service: UserService;
+    let userService: UserService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [UserService],
+            providers: [
+                UserService,
+                {
+                    provide: UserRepository,
+                    useClass: Repository, // Mock typeorm repository
+                },
+            ],
         }).compile();
 
-        service = module.get<UserService>(UserService);
+        userService = module.get<UserService>(UserService);
     });
 
-    it('should be defined', () => {
-        expect(service).toBeDefined();
+    describe('create', () => {
+        it('should return created user', async () => {
+            const user: User = {
+                username: 'username',
+                email: 'email@gmail.com',
+                password: 'password',
+                bio: 'bio',
+            };
+            const spy = jest.spyOn(userService, 'create').mockResolvedValue({
+                ...user,
+                id: 'id',
+            });
+
+            // Act
+            const result = await userService.create(user);
+
+            // Assert
+            expect(result.id).toBeTruthy();
+            expect(spy).toHaveBeenCalledWith(user);
+        });
+    });
+
+    describe('getAll', () => {
+        it('should return list of users', async () => {
+            const user: User = {
+                username: 'username',
+                email: 'email@gmail.com',
+                password: 'password',
+                bio: 'bio',
+            };
+            const users: User[] = [user, user, user];
+            const spy = jest
+                .spyOn(userService, 'getAll')
+                .mockResolvedValue(users);
+
+            // Act
+            const result = await userService.getAll();
+
+            // Assert
+            expect(result).toEqual(users);
+            expect(spy).toHaveBeenCalled();
+        });
     });
 });

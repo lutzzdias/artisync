39709548da40f3d69f900646bdf19d82a93c7810
lutzzdias <thiagolutzdias@hotmail.com>
2023-11-13 01:23:10 +0000

    feat [server]: define article entity and createDto

--- server/src/modules/article/dto/create-article.dto.ts
+++ server/src/modules/article/dto/create-article.dto.ts
@@ -1,6 +1,37 @@
-import { IsDate, IsOptional } from 'class-validator';
+import {
+    IsDate,
+    IsNotEmpty,
+    IsOptional,
+    IsString,
+    IsUUID,
+    MaxLength,
+} from 'class-validator';
 
 export class CreateArticleDto {
+    @IsString()
+    @IsNotEmpty()
+    title: string;
+
+    @IsOptional()
+    @IsString()
+    @MaxLength(510)
+    description?: string;
+
+    @IsOptional()
+    @IsString()
+    link?: string;
+
+    @IsString()
+    @IsNotEmpty()
+    author: string;
+
+    @IsUUID()
+    userId: string; // FK
+
+    // statusId: string; // FK
+
+    // projectId?: string; // FK
+
     @IsOptional()
     @IsDate()
     createdAt: Date;
--- server/src/modules/article/entity/article.entity.ts
+++ server/src/modules/article/entity/article.entity.ts
@@ -1,5 +1,12 @@
 export class Article {
     id?: string; // Optional to allow db to generate id
+    title: string;
+    description?: string;
+    link?: string;
+    author: string;
+    userId: string; // FK
+    // statusId: string; // FK
+    // projectId?: string; // FK
     createdAt?: Date;
     updatedAt?: Date;
 }

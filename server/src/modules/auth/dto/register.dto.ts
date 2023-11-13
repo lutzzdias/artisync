125bdccb394b79a47f51fab1cc572f6297c1b915
lutzzdias <thiagolutzdias@hotmail.com>
2023-11-13 05:20:41 +0000

    feat [server]: add validation to registerDto

--- server/src/modules/auth/dto/register.dto.ts
+++ server/src/modules/auth/dto/register.dto.ts
@@ -1,6 +1,28 @@
-// TODO: Add nest decorators
+import {
+    IsEmail,
+    IsLowercase,
+    IsNotEmpty,
+    IsString,
+    IsStrongPassword,
+} from 'class-validator';
+
 export class RegisterDto {
-    email: string;
+    @IsNotEmpty()
+    @IsLowercase()
+    @IsString()
     username: string;
+
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    @IsStrongPassword({
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 1,
+        minSymbols: 1,
+        minNumbers: 1,
+    })
     password: string;
 }

2fad10d0a966e33255c25b64b42051ee8faef280
lutzzdias <thiagolutzdias@hotmail.com>
2024-02-23 11:11:32 +0000

    feat: example page / components structure

---
+++ client/src/app/login/components/Title.tsx
@@ -0,0 +1,11 @@
+type TitleProps = {
+    str: string;
+}
+
+export function Title({ str }: TitleProps) {
+    return (
+        <h1>{str}</h1>
+    )
+}
+
+
---
+++ client/src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import { Title } from '@app/login/components/Title'
+
+export default function Login() {
+  return (
+    <div className="flex flex-col gap-5">
+            <Title str="Welcome to our platform" />
+    </div>
+  )
+}

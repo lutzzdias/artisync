f86533ae5950c871183fe0a1bcbab5a072e539e9
lutzzdias <thiagolutzdias@hotmail.com>
2024-06-19 02:10:58 +0100

    fix [client]: update next image config

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["media.istockphoto.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media.istockphoto.com",
+      },
+    ],
   },
 };
 
